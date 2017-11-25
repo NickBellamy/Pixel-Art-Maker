e9992a85dc0e132641cda16dbf4d888c36f388e6
Nick Bellamy <nick.bellamy@gmail.com>
2017-11-25 11:43:33 +0000

    feat: Call makeGrid() with user values on form submit
    Log to console for testing purposes in makeGrid() to verify that the values for gridHeight and gridWidth have been successfully passed.  event.PreventDefault() is called on submit too to prevent the page from reloading on submit.

--- designs.js
+++ designs.js
@@ -5,12 +5,15 @@ $("#colorPicker").change(function() {
     colorChoice = $(this).val();
 });
 
-// Select size input
+// On form Submit pass user values for height and width to makeGrid()
+$("#sizePicker").submit(function(event) {
+    var gridHeight = $("#input_height").val();
+    var gridWidth = $("#input_width").val();
+    makeGrid(gridHeight, gridWidth);
+    event.preventDefault();
+});
 
 // When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-    // Your code goes here!
-
+function makeGrid(gridHeight, gridWidth) {
+    console.log("Grid Height: " + gridHeight + ", GridWidth: " + gridWidth);
 }
