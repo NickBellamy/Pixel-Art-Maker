a6ea4af0e073462753cdc724fa875d617625b5c0
Nick Bellamy <nick.bellamy@gmail.com>
2017-11-25 11:20:14 +0000

    feat: Store user's color choice in var
    This colour is stored every time the user changes their seleciton in the color picker.

--- designs.js
+++ designs.js
@@ -1,10 +1,16 @@
 // Select color input
+var colorChoice;
+
+$("#colorPicker").change(function() {
+    colorChoice = $(this).val();
+});
+
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
-// Your code goes here!
+    // Your code goes here!
 
 }
