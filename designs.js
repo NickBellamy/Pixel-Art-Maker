18d9df7bb92a0bdbe5b7d5d807597f72a22c5e75
Nick Bellamy <nick.bellamy@gmail.com>
2017-11-25 12:48:10 +0000

    style: Adjust spacing and add more meaningful comments

--- designs.js
+++ designs.js
@@ -1,15 +1,17 @@
-// Select color input
+// var to store user's colour selection, initialized to default colorPicker value
 var colorChoice = $("#colorPicker").val();
 
+// Update colorChoice when new colour is picked
 $("#colorPicker").change(function() {
     colorChoice = $(this).val();
 });
 
+// Fill "pixel" with the currently selected colour
 $("#pixel_canvas").on("click", "td", function() {
     $(this).css("background-color", colorChoice);
 });
 
-// On form Submit pass user values for height and width to makeGrid()
+// Clears grid and passes user defined parameters to makeGrid()
 $("#sizePicker").submit(function(event) {
     var gridHeight = $("#input_height").val();
     var gridWidth = $("#input_width").val();
@@ -23,15 +25,15 @@ function clearGrid() {
     $("#pixel_canvas").empty();
 }
 
-// When size is submitted by the user, call makeGrid()
+// Draws grid using passed parameters
 function makeGrid(gridHeight, gridWidth) {
-  var output;
-  for (var i = 0; i < gridHeight; i++) {
-      output += "<tr>";
-      for (var j = 0; j < gridWidth; j++) {
-          output += "<td></td>";
-      }
-      output += "</tr>";
-  }
+    var output;
+    for (var i = 0; i < gridHeight; i++) {
+        output += "<tr>";
+        for (var j = 0; j < gridWidth; j++) {
+            output += "<td></td>";
+        }
+        output += "</tr>";
+    }
     $("#pixel_canvas").append(output);
 }
