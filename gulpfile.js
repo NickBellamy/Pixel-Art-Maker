18eb0a2978e00e9fa19ce7566eb637906ddc2c0c
Nick Bellamy <nick.bellamy@gmail.com>
2017-12-29 14:38:13 +0000

    Setup browser-sync
    Also add comments to gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
+// Default task
+gulp.task('default', ['serve']);
+
+// Set up server and watch scss files
+gulp.task('serve', ['styles'], function(){
+  browserSync.init({
+    server: "./"
+  });
+  
   gulp.watch('sass/**/*.scss', ['styles']);
 });
 
+// Compile sass into CSS, add vendor prefixes, and sync with browser
 gulp.task('styles', function(){
   gulp.src('sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions']
     }))
-    .pipe(gulp.dest('./css'));
-});
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+}); 
