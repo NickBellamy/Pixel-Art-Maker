1f53d57c1f69ee80bf2b6d44b6d2510aefb723eb
Nick Bellamy <nick.bellamy@gmail.com>
2018-01-01 14:59:27 +0000

    Add ESLinting to gulp file
    This shows errors in the terminal whenever a js file is changed.

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,27 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
+var eslint = require('gulp-eslint');
 
 // Default task
 gulp.task('default', ['serve']);
+
+gulp.task('lint', () => {
+    // ESLint ignores files with "node_modules" paths. 
+    // So, it's best to have gulp ignore the directory as well. 
+    // Also, Be sure to return the stream from the task; 
+    // Otherwise, the task may end before the stream has finished. 
+    return gulp.src(['**/*.js','!node_modules/**'])
+    // eslint() attaches the lint output to the "eslint" property 
+    // of the file object so it can be used by other modules. 
+        .pipe(eslint())
+    // eslint.format() outputs the lint results to the console. 
+    // Alternatively use eslint.formatEach() (see Docs). 
+        .pipe(eslint.format())
+    // To have the process exit with an error code (1) on 
+    // lint error, return the stream and pipe to failAfterError last. 
+        .pipe(eslint.failAfterError());
+});
 
 // Set up server and watch scss files
 gulp.task('serve', ['styles'], function(){
@@ -15,10 +33,11 @@ gulp.task('serve', ['styles'], function(){
     });
   
     gulp.watch('sass/**/*.scss', ['styles']);
+    gulp.watch('*.js', ['lint']);
 });
 
 // Compile sass into CSS, add vendor prefixes, and sync with browser
-gulp.task('styles', function(){
+gulp.task('styles', ['lint'], function(){
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
